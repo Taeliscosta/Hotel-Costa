c82094dcc8e8e712463d910c14b9cb63e9c5478b
Taélis Holanda <taeliscosta11@gmail.com>
2026-05-25 12:52:26 -0300

    implementa CRUD completo de clientes

--- backend/src/controllers/clienteController.js
+++ backend/src/controllers/clienteController.js
@@ -22,7 +22,56 @@ const cadastrarCliente = (req, res) => {
   });
 };
 
+const atualizarCliente = (req, res) => {
+  const {id} = req.params;
+
+  const{nome, email, telefone} = req.body;
+
+  const cliente = clientes.find(
+    cliente => cliente.id == id
+  );
+
+  if (!cliente) {
+    return res.status(404).json({
+      mensagem: "Cliente não encontrado"
+    });
+  }
+
+  cliente.nome = nome;
+  cliente.email = email;
+  cliente.telefone = telefone;
+
+  res.json({
+    mensagem: "Cliente atualizado com sucesso",
+    cliente
+  });
+};
+
+const deletarCliente = (req, res) => {
+  const {id} = req.params;
+
+  const clienteExiste = clientes.find(
+    cliente => cliente.id == id
+  );
+
+  if (!clienteExiste) {
+    return res.status(404).json({
+      mensagem: "Cliente não encontrado"
+    });
+  }
+
+  clientes = clientes.filter(
+    cliente => cliente.id != id
+  );
+
+  res.json({
+    mensagem: "Cliente removido com sucesso"
+  });
+};
+
 module.exports = {
   listarClientes,
-  cadastrarCliente
+  cadastrarCliente,
+  atualizarCliente,
+  deletarCliente
 };
--- backend/src/routes/clienteRoutes.js
+++ backend/src/routes/clienteRoutes.js
@@ -4,11 +4,17 @@ const router = express.Router();
 
 const {
   listarClientes, 
-  cadastrarCliente
+  cadastrarCliente,
+  atualizarCliente,
+  deletarCliente
 } = require("../controllers/clienteController");
 
 router.get("/", listarClientes);
 
 router.post("/", cadastrarCliente);
+
+router.put("/:id", atualizarCliente);
+
+router.delete("/:id", deletarCliente);
 
 module.exports = router;
