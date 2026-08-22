b2324aeadc045c8fe6a88a50631ba0e6b676497a
Taélis Holanda <taeliscosta11@gmail.com>
2026-08-21 22:25:51 -0300

    feat: adiciona cors e rota mock de login para o front

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,22 @@ app.use(express.json());
 
 app.use(logger);
 
+const usuarios = [
+  {id: 1, nome: 'Taélis Costa', email: 'taeliscosta11@gmail.com', senha: '12345678'} 
+];
+
+app.post("/login", (req, res) => {
+  const { email, senha } = req.body;
+  const usuario = usuarios.find((u) => u.email === email && u.senha === senha);
+
+  if (!usuario) {
+    return res.status(401).json({ message: 'E-mail ou senha inválidos.'});
+  }
+
+  const {senha: _, ...semSenha} = usuario;
+  res.json(semSenha);
+});
+
 app.use("/clientes", clienteRoutes);
 
 app.use("/quartos", quartoRoutes);
